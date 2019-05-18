3d6b8d6023d69731151a7a04934c39eaf68a11db
tanyujie <13551259347@139.com>
2019-05-19 00:45:08 +0800

    init

---
+++ turbine-vkwit/src/main/java/org/easymis/vkwit/turbine/TurbineVkwitApplication.java
@@ -0,0 +1,26 @@
+package org.easymis.vkwit.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableTurbine
+@EnableHystrixDashboard
+@Controller
+public class TurbineVkwitApplication {
+	@RequestMapping("/")
+	public String home() {
+		return "forward:/hystrix";
+	}
+	public static void main(String[] args) {
+
+		SpringApplication.run(TurbineVkwitApplication.class, args);
+	}
+
+}
