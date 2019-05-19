e3e376f069cd8f1cf2235f4333d4fbb8603a0511
tanyujie <13551259347@139.com>
2019-05-19 10:51:58 +0800

    init

---
+++ zuul-vkwit/src/main/java/org/easymis/vkwit/zuul/TokenFilter.java
@@ -0,0 +1,55 @@
+package org.easymis.vkwit.zuul;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class TokenFilter extends ZuulFilter{
+	private final Logger logger = LoggerFactory.getLogger(TokenFilter.class);
+	@Override
+	public boolean shouldFilter() {
+		return true; //表示是否需要执行该filter，true表示执行，false表示不执行
+	}
+	//filter需要执行的具体操作
+	@Override
+	public Object run() throws ZuulException {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+ 
+        logger.info("--->>> TokenFilter {},{}", request.getMethod(), request.getRequestURL().toString());
+ 
+        String token = request.getParameter("token");// 获取请求的参数
+ 
+        if (StringUtils.isNotBlank(token)) {
+            ctx.setSendZuulResponse(true); //对请求进行路由
+            ctx.setResponseStatusCode(200);
+            ctx.set("isSuccess", true);
+            return null;
+        } else {
+            ctx.setSendZuulResponse(false); //不对其进行路由
+            ctx.setResponseStatusCode(400);
+            ctx.setResponseBody("token is empty");
+            ctx.set("isSuccess", false);
+            return null;
+        }
+
+	}
+
+	@Override
+	public String filterType() {
+		return "pre"; //定义filter的类型，有pre、route、post、error四种
+	}
+
+	@Override
+	public int filterOrder() {
+		return 10; //定义filter的顺序，数字越小表示顺序越高，越先执行
+	}
+
+}
--- zuul-vkwit/src/main/java/org/easymis/vkwit/zuul/ZuulApplication.java
+++ zuul-vkwit/src/main/java/org/easymis/vkwit/zuul/ZuulApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +13,11 @@ public class ZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
+	}
+	//将TokenFilter加入到请求拦截队列
+	@Bean
+	public TokenFilter tokenFilter() {
+		return new TokenFilter();
 	}
 
 }
